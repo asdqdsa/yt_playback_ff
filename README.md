e9108dc01f242b499f9d7b03e1bf00fcc6463acd
asdqdsa <asdqdsa@protonmail.com>
2023-05-12 00:04:49 +0400

    refactor: refactor

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Youtube Playback Speed Extansion for Firefox
 
-## Tested version: Firefox [Version 103.0.1 (64-bit) (64-bit)]
+## Tested version: Firefox [Version 103.0.1 (64-bit)]
 
 cd "PROJECT_DIRECTORY_FOLDER" \
 git clone https://github.com/asdqdsa/yt_playback_ff \
-Go To Firefox Extension in your browser \
+Go To about:addons in your browser \
 Click Options -> debug Add-ons \
 Click Load Temporary Add-on... \
 Select JSON file *manifest.json* within cloned folder \
 Open and play any youtube video [https://www.youtube.com/] \
-Change speed of the video with extension \
+Change speed of the video with extension
 
 
-### Known bugs
+## Known bugs
 Current speed won't update if speed changes \
--- popup/popup.js
+++ popup/popup.js
@@ -1,30 +1,59 @@
-let speedUp = document.getElementById("speedUp");
-let slowDown = document.getElementById("slowDown");
-let currentSpeed = document.getElementById("currentSpeed");
+const speedUp = document.getElementById('speedUp');
+const slowDown = document.getElementById('slowDown');
+const currentSpeed = document.getElementById('currentSpeed');
+const popUp = document.getElementById('popUp');
+const increment = 0.25;
 
-window.onload = async function() {
+window.addEventListener('load', loadHandler);
+async function loadHandler() {
   console.log('window onload');
-  let [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+  const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
   browser.scripting.executeScript({
     target: { tabId: tab.id },
-    func: () => browser.storage.local.set({actaulSpeed: document.getElementsByTagName("video")[0].playbackRate}),
+    func: windowHandler,
   });
-  let { actaulSpeed }  = await browser.storage.local.get(['actaulSpeed']);
+  const { actaulSpeed } = await browser.storage.local.get(['actaulSpeed']);
   currentSpeed.textContent = actaulSpeed;
 }
 
-speedUp.addEventListener("click", async () => {
-	let [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-  browser.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => console.log(document.getElementsByTagName("video")[0].playbackRate += 0.5)
+function windowHandler() {
+  browser.storage.local.set({
+    actaulSpeed: document.getElementsByTagName('video')[0].playbackRate,
   });
+}
+
+slowDown.addEventListener('click', () => setSpeed(increment));
+speedUp.addEventListener('click', (event) => {
+  if (event.target.id === 'speedUp') {
+    setSpeed(increment * -1);
+  }
 });
 
-slowDown.addEventListener("click", async () => {
-	let [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+async function setSpeed(increment) {
+  const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
   browser.scripting.executeScript({
     target: { tabId: tab.id },
-    func: () => console.log(document.getElementsByTagName("video")[0].playbackRate -= 0.5)
+    args: [increment],
+    func: handlerDown,
   });
-});
+}
+
+async function handlerDown(...step) {
+  // localStorage.setItem(['sanicStep'], JSON.stringify({ sanicStep: step }));
+  // const json = JSON.parse(localStorage.getItem('sanicStep'));
+  document.getElementsByTagName('video')[0].playbackRate -= step;
+  const curSpeed = document.getElementsByTagName('video')[0].playbackRate;
+  console.log(curSpeed);
+  localStorage.setItem(
+    ['sanicSpeed'],
+    JSON.stringify({ sanicSpeed: curSpeed })
+  );
+}
+
+function delagate(el, selector, eventKey, handler) {
+  el.addEventListener(eventKey, (event) => {
+    if (event.target.matches(selector)) {
+      handler(event.target);
+    }
+  });
+}
